87c305f8a7e396959db9f295e09e8e43d1fcad04
William Ta <wta@student.42.fr>
2019-12-16 17:15:00 +0100

    feat: add coefficient denormalization

--- train.py
+++ train.py
@@ -28,37 +28,19 @@ def main():
 	x_normalized = normalize_array(x_data)
 	y_normalized = normalize_array(y_data)
 
-	learning_rate = 0.0001
-	epsilon = 0.0001
+	learning_rate = 0.1
 
-	b = 1
+	b = 0
 	m = 1
-	while True:
-		new_b, new_m = stepGradient(learning_rate, b, m, x_normalized, y_normalized)
-		if (-epsilon <= abs(b - new_b) <= epsilon and -epsilon <= abs(m - new_m) <= epsilon):
-			print(new_b, new_m)
-			return
-		else:
-			b = new_b
-			m = new_m
-
-	# mileages, prices = list(dataset.keys()), list(dataset.values())
-
-	# mileageMean = np.mean(mileages)
-	# priceMean = np.mean(prices)
-	# sumErrors = 0
-	# sumSquaredMileageErrors = 0
-	# for i in range (0, len(mileages)):
-	# 	m = mileages[i] - mileageMean
-	# 	sumSquaredMileageErrors += m ** 2
-	# 	sumErrors += m * (prices[i] - priceMean)
-	# slope = sumErrors / sumSquaredMileageErrors
-	# intercept = priceMean - slope * mileageMean
-	# print(slope, intercept)
-	# x = np.linspace(0, 300000, 10000)
-	# y = slope * x + intercept
-	# plt.plot(x, y, 'r')
-	# plt.plot(mileages, prices)
+	x = np.array([0, 1])
+	for i in range(0, 1000):
+		b, m = stepGradient(learning_rate, b, m, x_normalized, y_normalized)
+	print(b, m)
+	m = m * (max(y_data) - min(y_data)) / (max(x_data) - min(x_data))
+	
+	# x = np.array([0, 1])
+	# y = m * x + b
+	# plt.plot(x, y)
 	# plt.show()
 
 if __name__ == '__main__':
