411fd7d0d500caa33be2c190a2b7d5c2c232cae0
William Ta <wta@student.42.fr>
2019-12-16 19:15:14 +0100

    feat: implement visualization with matplotlib

--- train.py
+++ train.py
@@ -3,17 +3,17 @@ import numpy as np
 import pandas as pd
 
 def stepGradient(learning_rate, b, m, x_data, y_data):
-	b_gradient = 0
-	m_gradient = 0
+	sum_intercept = 0
+	sum_slope = 0
 	N = len(x_data)
 	for i in range(0, N):
-		guess = m * x_data[i] + b
-		error = y_data[i] - guess
-		b_gradient += -(2 / N) * error
-		m_gradient += -(2 / N) * x_data[i] * error
-	new_b = b - (learning_rate * b_gradient)
-	new_m = m - (learning_rate * m_gradient)
-	return [new_b, new_m]
+		estimation = m * x_data[i] + b
+		error = estimation - y_data[i]
+		sum_intercept += error
+		sum_slope += error * x_data[i]
+	theta0 = learning_rate * (sum_intercept / N)
+	theta1 = learning_rate * (sum_slope / N)
+	return [b - theta0, m - theta1]
 
 def normalize_array(array):
 	return (array - min(array)) / (max(array) - min(array))
@@ -22,19 +22,29 @@ def denormalize(value, array):
 	return value * (max(array) - min(array)) + min(array)
 
 def main():
-	data_train = pd.read_csv("data.csv")
-	x_data = np.array(data_train['km'])
-	y_data = np.array(data_train['price'])
+	try:
+		data_train = pd.read_csv("data.csv")
+		x_data = np.array(data_train['km'])
+		y_data = np.array(data_train['price'])
+	except ValueError:
+		print("error: invalid datas")
 	x_normalized = normalize_array(x_data)
 	y_normalized = normalize_array(y_data)
 
-	learning_rate = 0.1
+	learning_rate = 1
 
 	b = 0
-	m = 1
+	m = 0
 	x = np.array([0, 1])
 	for i in range(0, 1000):
 		b, m = stepGradient(learning_rate, b, m, x_normalized, y_normalized)
+		x = np.array([0, 1])
+		y = m * x + b
+		plt.plot(x_normalized, y_normalized, 'ro')
+		plt.plot(x, y, 'b')
+		plt.draw()
+		plt.pause(0.0000001)
+		plt.cla()
 	m = m * (max(y_data) - min(y_data)) / (max(x_data) - min(x_data))
 	b = denormalize(b, y_data) + min(x_data) * -m
 	print(b, m)
