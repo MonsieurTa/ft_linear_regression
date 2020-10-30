2888e758e74cb2efc2e07134d51ecc20620cb468
wta <wta@student.42.fr>
2020-10-30 16:18:31 +0100

    refactor: use matplotlib animate

--- train.py
+++ train.py
@@ -1,6 +1,49 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import pandas as pd
+
+def main():
+	try:
+		data_train = pd.read_csv("data.csv")
+		x_data = np.array(data_train['km'])
+		y_data = np.array(data_train['price'])
+	except ValueError:
+		print("error: invalid datas")
+
+	x = np.array([0, 1])
+	y_result = np.empty((0, 2))
+	x_normalized = normalize_array(x_data)
+	y_normalized = normalize_array(y_data)
+	learning_rate = 1
+	b = 0
+	m = 0
+
+	for _ in range(1000):
+		b, m = stepGradient(learning_rate, b, m, x_normalized, y_normalized)
+		y = m * x + b
+		y_result = np.append(y_result, [y], axis=0)
+
+	m = m * (max(y_data) - min(y_data)) / (max(x_data) - min(x_data))
+	b = denormalize(b, y_data) + min(x_data) * -m
+	print(b, m)
+
+	# PLOT
+	fig, ax = plt.subplots()
+	ax.plot(x_normalized, y_normalized, 'ro')
+	line, = ax.plot([], [], 'b')
+
+	def init():
+		line.set_xdata(x)
+		line.set_ydata([])
+		return line,
+
+	def update(i):
+		line.set_ydata(y_result[i])
+		return line,
+
+	ani = animation.FuncAnimation(fig, update, init_func=init, frames=1000, interval=50, repeat=False, blit=True)
+	plt.show()
 
 def stepGradient(learning_rate, b, m, x_data, y_data):
 	sum_intercept = 0
@@ -20,34 +63,6 @@ def normalize_array(array):
 
 def denormalize(value, array):
 	return value * (max(array) - min(array)) + min(array)
-
-def main():
-	try:
-		data_train = pd.read_csv("data.csv")
-		x_data = np.array(data_train['km'])
-		y_data = np.array(data_train['price'])
-	except ValueError:
-		print("error: invalid datas")
-	x_normalized = normalize_array(x_data)
-	y_normalized = normalize_array(y_data)
-
-	learning_rate = 1
-
-	b = 0
-	m = 0
-	x = np.array([0, 1])
-	for i in range(0, 1000):
-		b, m = stepGradient(learning_rate, b, m, x_normalized, y_normalized)
-		x = np.array([0, 1])
-		y = m * x + b
-		plt.plot(x_normalized, y_normalized, 'ro')
-		plt.plot(x, y, 'b')
-		plt.draw()
-		plt.pause(0.0000001)
-		plt.cla()
-	m = m * (max(y_data) - min(y_data)) / (max(x_data) - min(x_data))
-	b = denormalize(b, y_data) + min(x_data) * -m
-	print(b, m)
 
 if __name__ == '__main__':
 	main()
